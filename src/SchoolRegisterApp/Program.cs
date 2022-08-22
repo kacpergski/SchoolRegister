b7c66c592cbd8c6b57dfde5fac43ab5491220783
Kacper <kacperg@gmail.com>
2022-08-22 22:25:44 +0200

    day5

--- src/SchoolRegisterApp/Program.cs
+++ src/SchoolRegisterApp/Program.cs
@@ -6,6 +6,14 @@ namespace SchoolRegisterApp
     {
         static void Main(string[] args)
         {
+            var grades = new [] {4.2, 5.0, 3.0};
+            double gradesSum = 0;
+            foreach(var grade in grades)
+            {
+                gradesSum += grade;
+                Console.WriteLine(grade);
+                Console.WriteLine();
+            }
             Console.WriteLine("Hello World!");
         }
     }
