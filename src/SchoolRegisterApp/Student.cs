310b2e5f9da21553db9e80b97fc4471d0e981d73
Kacper <41369418+kacpergski@users.noreply.github.com>
2022-08-23 11:47:03 +0200

    Update Student.cs

--- src/SchoolRegisterApp/Student.cs
+++ src/SchoolRegisterApp/Student.cs
@@ -1,11 +1,11 @@
 namespace SchoolRegisterApp
 {
-    internal class Student
+    public class Student
     { 
         private string name;  //pole prywatne do przechowywania imion
         public Student(string name)
         {
-            this.name = name; // słowo this aby przypisac name z konstruktora (to co w nawiasie) do pola name
+            this.name = name; // słowo this aby odwołać sie do klasy i przypisac name z konstruktora (to co w nawiasie) do pola name
         }
     }
 }
