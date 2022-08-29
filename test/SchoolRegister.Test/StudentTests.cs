68779fe53ea90302e5f2647146df23028fff11bf
Kacper <41369418+kacpergski@users.noreply.github.com>
2022-08-29 16:14:42 +0200

    v31

---
+++ test/SchoolRegister.Test/StudentTests.cs
@@ -0,0 +1,36 @@
+using SchoolRegisterApp;
+namespace SchoolRegister.Test;
+
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+//arange
+var student1 = new Student("Kacper");
+student1.AddGrade(3.0);
+student1.AddGrade(3.0);
+student1.AddGrade(3.0);
+
+// var a = 4;
+// var b = 5;
+// var excepted = 9;
+
+//act
+
+var result = student1.GetStatistic();
+
+//var result = a + b;  
+
+//assert
+//var excepted = 4.0;
+ //Assert.Equal(excepted,result);
+
+Assert.Equal(3.0,result.average);
+Assert.Equal(5.0,result.maxGrade);
+Assert.Equal(3.0,result.minGrade);
+
+
+    }
+}
